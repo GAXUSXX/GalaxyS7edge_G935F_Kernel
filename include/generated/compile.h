0cb65621c3ade5c627eaade2159d4038da7a6a1f
GAXUSXX <mchnet3@gmail.com>
2016-03-17 00:23:11 +0900

    gpu: arm: Add Exynos Mali voltage control

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 65 */
+/* This file is auto generated, version 67 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#65 SMP PREEMPT Wed Mar 16 23:00:48 JST 2016"
+#define UTS_VERSION "#67 SMP PREEMPT Thu Mar 17 00:13:24 JST 2016"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "gaxus-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.9.x-google 20150123 (prerelease) (GCC) "
