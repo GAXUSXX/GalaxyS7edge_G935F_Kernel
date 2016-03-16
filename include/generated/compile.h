b1474ccb670446bdc61d3ef161af0f9e92b0ab22
GAXUSXX <mchnet3@gmail.com>
2016-03-17 01:50:00 +0900

    gpu: tunning voltage

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 71 */
+/* This file is auto generated, version 72 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#71 SMP PREEMPT Thu Mar 17 01:24:33 JST 2016"
+#define UTS_VERSION "#72 SMP PREEMPT Thu Mar 17 01:38:50 JST 2016"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "gaxus-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.9.x-google 20150123 (prerelease) (GCC) "
