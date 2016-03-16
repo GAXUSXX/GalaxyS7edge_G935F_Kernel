5828a2d302fe50f639858ea6c3f028fd4249f44c
GAXUSXX <mchnet3@gmail.com>
2016-03-17 00:32:40 +0900

    gpu: tunning freq for better gaming performance OC

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 67 */
+/* This file is auto generated, version 68 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#67 SMP PREEMPT Thu Mar 17 00:13:24 JST 2016"
+#define UTS_VERSION "#68 SMP PREEMPT Thu Mar 17 00:28:03 JST 2016"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "gaxus-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.9.x-google 20150123 (prerelease) (GCC) "
