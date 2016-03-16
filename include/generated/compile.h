411d02e2099fc0ff4f14f69a8caedd45a43f2b1b
GAXUSXX <mchnet3@gmail.com>
2016-03-17 02:16:26 +0900

    add d2w and sweep2sleep

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 74 */
+/* This file is auto generated, version 75 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#74 SMP PREEMPT Thu Mar 17 02:06:14 JST 2016"
+#define UTS_VERSION "#75 SMP PREEMPT Thu Mar 17 02:14:55 JST 2016"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "gaxus-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.9.x-google 20150123 (prerelease) (GCC) "
