5dd545edb3144fcf5eb17e3abdaf67fda8127d11
GAXUSXX <mchnet3@gmail.com>
2016-03-17 02:10:33 +0900

    Powersuspend

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 72 */
+/* This file is auto generated, version 74 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm64"
-#define UTS_VERSION "#72 SMP PREEMPT Thu Mar 17 01:38:50 JST 2016"
+#define UTS_VERSION "#74 SMP PREEMPT Thu Mar 17 02:06:14 JST 2016"
 #define LINUX_COMPILE_BY "root"
 #define LINUX_COMPILE_HOST "gaxus-VirtualBox"
 #define LINUX_COMPILER "gcc version 4.9.x-google 20150123 (prerelease) (GCC) "
